bbc4eab2617620e0802b84e4caccbb7e7c0d0e08
Richard M. <richardm246@nycstudents.net>
2024-10-02 10:32:07 -0400

    Consider separating auth.js and storage.js, with cloudinary middleware in item controller with multer

--- backend/storage.js
+++ backend/storage.js
@@ -1,44 +1,22 @@
-import { v2 as cloudinary } from "cloudinary";
+const cloudinary = require("cloudinary");
 
 (async function () {
-  cloudinary.config({
-    cloud_name: "dbrjbu4kt",
-    api_key: "392916843562773",
+  cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
   });
 
-  uploadResult = (path, folder) => {
-    cloudinary.uploader
-      .upload(
-        "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-        {
-          public_id: "shoes",
-        }
-      )
-      .then((data) => {
-        return { url: data.url, public_id: data.public_id };
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "CloudinaryDemo",
+      allowedFormats: ["jpeg", "png", "jpg"],
+    },
+  });
+
+  module.exports = {
+    storage,
   };
-
-  console.log(uploadResult);
-
-  const optimizeUrl = cloudinary.url("shoes", {
-    fetch_format: "auto",
-    quality: "auto",
-  });
-
-  console.log(optimizeUrl);
-
-  const autoCropUrl = cloudinary.url("shoes", {
-    crop: "auto",
-    gravity: "auto",
-    width: 500,
-    height: 500,
-  });
-
-  console.log(autoCropUrl);
-})();
-module.exports = { uploadResult };
+});
