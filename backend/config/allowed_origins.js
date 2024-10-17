1b60946f39f1a5435389b11ca722d0695ebeaabd
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-10-17 10:58:14 -0400

    Update allowed_origins.js

--- backend/config/allowed_origins.js
+++ backend/config/allowed_origins.js
@@ -3,6 +3,7 @@ const origins = [
   "http://localhost:3500",
   "http://localhost:8080",
   "http://localhost:3000",
+  "http://localhost:5173",
   "http://127.0.0.1:5173", //vue
 ];
 module.exports = origins;
