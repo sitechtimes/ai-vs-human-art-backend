7248447a39adb52d00917e90f73aa114c727831c
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-02-28 10:56:32 -0500

    rfdgdg

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -3,12 +3,12 @@ const Game = require("../models/gameModel");
 async function saveGame(req, res) {
   const { right, total, user } = req.body;
 
-  if (!right || !total || !user) {
+  if (!right || !total) {
     return res.status(422).json({ message: "Invalid fields" });
   }
   await Game.create({
     right,
-    wrong,
+    total,
     time: Date.now(),
     user,
   });
