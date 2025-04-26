3130a8fe3915e29d81a8feea52b23ceaf568e6af
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2025-04-25 21:22:21 -0400

    Update allowed_origins.js

--- backend/config/allowed_origins.js
+++ backend/config/allowed_origins.js
@@ -5,6 +5,7 @@ const origins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://ai-vs-human-art-backend.onrender.com",
+  "https://ai-v-humans.netlify.app",
   "http://127.0.0.1:5173", //vue
 ];
 module.exports = origins;
