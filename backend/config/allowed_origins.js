7dc46d313be8ff6ceac329232b087b80b095e033
katherine <katherineze26@gmail.com>
2025-09-01 01:18:16 -0400

    samd

--- backend/config/allowed_origins.js
+++ backend/config/allowed_origins.js
@@ -1,11 +1,11 @@
 const origins = [
-  // "http://127.0.0.1:5000",
-  // "http://localhost:3500",
-  // "http://localhost:8080",
-  // "http://localhost:3000",
-  // "http://localhost:5173",
+  "http://127.0.0.1:5000",
+  "http://localhost:3500",
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://localhost:5173",
   "https://ai-vs-human-art-backend-6fgg.onrender.com",
   "https://artagainstthemachine.netlify.app",
-  // "http://127.0.0.1:5173", //vue
+  "http://127.0.0.1:5173", //vue
 ];
 module.exports = origins;
