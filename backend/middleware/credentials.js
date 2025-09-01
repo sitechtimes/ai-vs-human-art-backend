8f29b75e8aa9510f8d3e0021d89f5bedcb5489d8
katherine <katherineze26@gmail.com>
2025-09-01 01:12:46 -0400

    da

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -4,9 +4,12 @@ const allowedOrigins = require("../config/allowed_origins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
+  if (allowedOrigins.includes(`https://artagainstthemachine.netlify.app/`)) {
     // allows allowed servers
-    res.header("Access-Control-Allow-Origin", origin); // gives it a cool header
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://artagainstthemachine.netlify.app/"
+    ); // gives it a cool header
     res.header("Access-Control-Allow-Credentials", true);
   }
 
