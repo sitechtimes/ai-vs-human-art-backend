3f0cebd8b677a4aee7359cce9335dcf0d109acf1
katherine <katherineze26@gmail.com>
2025-09-01 00:51:43 -0400

    asdasd

--- backend/config/allowed_origins.js
+++ backend/config/allowed_origins.js
@@ -4,8 +4,8 @@ const origins = [
   "http://localhost:8080",
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://ai-vs-human-art-backend.onrender.com",
-  "https://ai-v-humans.netlify.app",
+  "https://ai-vs-human-art-backend-6fgg.onrender.com/",
+  "https://artagainstthemachine.netlify.app/game",
   "http://127.0.0.1:5173", //vue
 ];
 module.exports = origins;
